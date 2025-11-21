db72b5561535056cc46d137759a6f870b9aedab4
Arshad Ullah <mdarshadullah835@gmail.com>
2025-11-21 22:47:36 +0600

    product api intrigated

--- src/dashboard/product/EditProduct.jsx
+++ src/dashboard/product/EditProduct.jsx
@@ -1,9 +1,161 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { Link, useParams } from "react-router";
 
+import { toast } from "react-toastify";
+import {
+  useGetSingleProductQuery,
+  useUpdateProductMutation,
+} from "../../redux/features/product/productApi";
+import { useGetCategoryQuery } from "../../redux/features/category/categoryApi";
 const EditProduct = () => {
+  const { id } = useParams();
+  const [updateProduct] = useUpdateProductMutation();
+  const { data: categories } = useGetCategoryQuery();
+
+  const { data } = useGetSingleProductQuery(id);
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  useEffect(() => {
+    if (data?.data) {
+      reset({
+        name: data?.data.name,
+        price: data?.data.price,
+        quantity: data?.data.quantity,
+        description: data?.data.description,
+        categoryId: data?.data.categoryId._id,
+        image: data?.data.image,
+      });
+    }
+  }, [data, reset]);
+  const onSubmit = async (data) => {
+    try {
+      const formData = new FormData();
+      formData.append("name", data.name);
+      formData.append("price", data.price);
+      formData.append("quantity", data.quantity);
+      formData.append("description", data.description);
+      formData.append("categoryId", data.categoryId);
+      if (data?.image && data.image[0]) formData.append("image", data.image[0]);
+      const result = await updateProduct({ id, data: formData }).unwrap();
+      console.log(result);
+      toast.success(result.message);
+      console.log(result);
+      reset();
+    } catch (err) {
+      console.log(err);
+      toast.error(err.message || "failed to create product");
+    }
+  };
+
+  const inputClass =
+    "w-full border border-accent p-2 rounded transition-all focus:border-primary focus:ring-2 focus:ring-primary focus:outline-none text-text";
+
   return (
-    <div>
-      <p>This is product edit page</p>
+    <div className="min-h-screen flex justify-center items-start bg-gray-50  ">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="bg-white shadow-md rounded-lg p-6 w-full  grid grid-cols-1 md:grid-cols-2 gap-6"
+      >
+        <h2 className="text-3xl font-bold text-center mb-3 text-primary col-span-1 md:col-span-2">
+          Edit Product
+        </h2>
+
+        {/* Category */}
+        <div>
+          <label className="block font-semibold mb-1 text-gray-700">
+            Category
+          </label>
+          <select {...register("categoryId")} className={inputClass}>
+            <option value="">Select a category</option>
+            {categories?.data?.map((cat) => (
+              <option key={cat._id} value={cat._id}>
+                {cat?.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        {/* Product Name */}
+        <div>
+          <label className="block font-semibold mb-1 text-gray-700">Name</label>
+          <input
+            {...register("name")}
+            className={inputClass}
+            placeholder="Enter product name"
+          />
+        </div>
+
+        {/* Price */}
+        <div>
+          <label className="block font-semibold mb-1 text-gray-700">
+            Price
+          </label>
+          <input
+            type="number"
+            {...register("price")}
+            className={inputClass}
+            placeholder="Enter price"
+          />
+        </div>
+
+        {/* Quantity */}
+        <div>
+          <label className="block font-semibold mb-1 text-gray-700">
+            Quantity
+          </label>
+          <input
+            type="number"
+            {...register("quantity")}
+            className={inputClass}
+            placeholder="Enter quantity"
+          />
+        </div>
+
+        {/* Description */}
+        <div className="md:col-span-2">
+          <label className="block font-semibold mb-1 text-gray-700">
+            Description
+          </label>
+          <textarea
+            {...register("description")}
+            className={inputClass + " h-24 resize-none"}
+            placeholder="Enter product description"
+          />
+        </div>
+
+        {/* Image */}
+        <div className="md:col-span-2">
+          <label className="block font-semibold mb-1 text-gray-700">
+            Image
+          </label>
+          <input type="file" {...register("image")} className={inputClass} />
+        </div>
+
+        {/* Submit */}
+        <button
+          type="submit"
+          className="w-full md:col-span-2 bg-primary text-white py-2 rounded-lg font-semibold hover:bg-secondary transition-all"
+        >
+          Edit Product
+        </button>
+
+        {/* Product List Link */}
+        <p className="text-center mt-4 text-sm text-gray-600 md:col-span-2">
+          View all Products?{" "}
+          <Link
+            to="/dashboard/products"
+            className="text-blue-600 font-semibold underline hover:text-primary"
+          >
+            Product List
+          </Link>
+        </p>
+      </form>
     </div>
   );
 };
--- src/dashboard/product/ProductList.jsx
+++ src/dashboard/product/ProductList.jsx
@@ -1,9 +1,134 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router";
+import { toast } from "react-toastify";
+import {
+  useDeleteProductMutation,
+  useGetProductQuery,
+} from "../../redux/features/product/productApi";
+import DeleteModal from "./../../components/DeleteModal";
 
 const ProductList = () => {
+  const { data } = useGetProductQuery();
+  console.log(data?.data);
+  const [deleteProduct, { isLoading }] = useDeleteProductMutation();
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [deleteItem, setDeleteItem] = useState(null);
+
+  const openModal = (item) => {
+    setDeleteItem(item);
+    setIsOpen(true);
+  };
+
+  const confirmDelete = async () => {
+    try {
+      const result = await deleteProduct(deleteItem).unwrap();
+      toast.success(result.message);
+      setIsOpen(false);
+    } catch (err) {
+      toast.error(err?.data?.message || "Something went wrong");
+    }
+  };
+
+  if (!data?.data || data?.data.length === 0) {
+    return (
+      <Link to="/dashboard/add-product" className="text-primary underline">
+        Back To Add Product
+      </Link>
+    );
+  }
+
   return (
-    <div>
-      <p>This is product list page</p>
+    <div className="min-h-screen bg-gray-100 px-4 py-8">
+      <h2 className="text-2xl font-semibold mb-6 text-gray-800">
+        Products List
+      </h2>
+
+      <div className="overflow-x-auto bg-white shadow rounded-lg">
+        <table className="min-w-full  border border-gray-300 text-left">
+          {/* Table Head */}
+          <thead className="bg-gray-200 text-center">
+            <tr>
+              <th className="px-4 py-3 text-sm font-medium text-gray-700 border border-gray-300">
+                Name
+              </th>
+              <th className="px-4 py-3 text-sm font-medium text-gray-700 border border-gray-300">
+                Price
+              </th>
+              <th className="px-4 py-3 text-sm font-medium text-gray-700 border border-gray-300">
+                Quantity
+              </th>
+              <th className="px-4 py-3 text-sm font-medium text-gray-700 border border-gray-300">
+                Category
+              </th>
+              <th className="px-4 py-3 text-sm font-medium text-gray-700 border border-gray-300">
+                Description
+              </th>
+              <th className="px-4 py-3 text-sm font-medium text-gray-700 border border-gray-300">
+                Image
+              </th>
+              <th className="px-4 py-3 text-sm font-medium text-gray-700 border border-gray-300 text-center">
+                Action
+              </th>
+            </tr>
+          </thead>
+
+          {/* Table Body */}
+          <tbody>
+            {data.data.map((item) => (
+              <tr
+                key={item._id}
+                className="hover:bg-gray-50 border border-gray-300 text-center"
+              >
+                <td className="px-4 py-2 text-gray-900 border border-gray-300">
+                  {item.name}
+                </td>
+                <td className="px-4 py-2 text-gray-900 border border-gray-300">
+                  {item.price}
+                </td>
+                <td className="px-4 py-2 text-gray-900 border border-gray-300">
+                  {item.quantity}
+                </td>
+                <td className="px-4 py-2 text-gray-900 border border-gray-300">
+                  {item?.categoryId?.name}
+                </td>
+                <td className="px-4 py-2 text-gray-600 text-sm border border-gray-300">
+                  {item.description}
+                </td>
+                <td className="px-4 py-2 border border-gray-300">
+                  <img
+                    src={item.image}
+                    alt="category"
+                    className="h-10 w-10 rounded object-cover border"
+                  />
+                </td>
+                <td className="px-4 py-2 flex justify-center items-center gap-2 ">
+                  <Link to={`/dashboard/edit-product/${item._id}`}>
+                    <button className="px-3 py-1 text-sm bg-blue-600 text-white rounded hover:bg-blue-700">
+                      Update
+                    </button>
+                  </Link>
+
+                  <button
+                    onClick={() => openModal(item._id)}
+                    className="px-3 py-1 text-sm bg-red-600 text-white rounded hover:bg-red-700"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <DeleteModal
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        onConfirm={confirmDelete}
+        title="Delete Confirmation"
+        message={`Are you sure you want to delete "${deleteItem?.name}"?`}
+      />
     </div>
   );
 };
