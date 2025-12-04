fe3bc6d16abc3a3f9d12ebf5af9e076b733458ac
Arshad Ullah <mdarshadullah835@gmail.com>
2025-12-04 19:50:47 +0600

    order page is redesigne

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -6,17 +6,22 @@ import { useCreateOrderMutation } from "../redux/features/order/order";
 import { toast } from "react-toastify";
 import { clearCart } from "../redux/features/cart/cart";
 
+import cash from "../assets/cash.jpg";
+import bkash from "../assets/bkash.svg";
+import nagad from "../assets/nagad.png";
+
 const OrderPage = () => {
   const [createOrder] = useCreateOrderMutation();
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { user } = useSelector((state) => state.auth);
-  console.log(user);
   const { cartItems, totalPrice } = useSelector((state) => state.cart);
 
-  const [deliveryMethod, setDeliveryMethod] = useState("Delivery");
-
-  const { register, handleSubmit, watch } = useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     defaultValues: {
       address: {
         name: user?.name || "",
@@ -25,7 +30,7 @@ const OrderPage = () => {
         upozilla: user?.address?.upozilla || "",
         city: user?.address?.city || "",
       },
-      paymentMethod: "Card",
+      paymentMethod: "Cash",
     },
   });
 
@@ -42,6 +47,7 @@ const OrderPage = () => {
         paymentMethod: data.paymentMethod,
         paymentStatus: "Pending",
       };
+
       const result = await createOrder(orderData).unwrap();
       toast.success(result.message || "Order created successfully");
       dispatch(clearCart());
@@ -59,8 +65,8 @@ const OrderPage = () => {
   }, [cartItems]);
 
   return (
-    <div className="max-w-7xl mx-auto p-4 md:p-8 min-h-screen">
-      <h1 className="text-3xl font-bold text-gray-800 mb-10 text-center md:text-left">
+    <div className="max-w-7xl mx-auto  min-h-screen">
+      <h1 className="text-3xl font-bold text-text py-10 text-center md:text-left">
         Checkout
       </h1>
 
@@ -68,57 +74,111 @@ const OrderPage = () => {
         {/* Shipping Form */}
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="bg-white rounded-2xl shadow-lg p-6 flex flex-col space-y-4"
+          className="bg-white rounded shadow p-6 flex flex-col space-y-4"
         >
-          <h2 className="text-xl font-semibold text-gray-800">
+          <h2 className="text-xl font-semibold text-text">
             Shipping Information
           </h2>
 
-          {/* Input fields */}
           <input
             {...register("address.name")}
             placeholder="Full Name"
             className={inputClass}
+            required
           />
           <input
             {...register("address.email")}
             placeholder="Email Address"
             className={inputClass}
+            required
           />
           <input
-            {...register("address.phone")}
+            {...register("address.phone", {
+              required: "Phone number is required",
+              pattern: {
+                value: /^(01[3-9]\d{8})$/,
+                message: "Please enter a valid Bangladeshi phone number",
+              },
+            })}
             placeholder="Phone Number"
             className={inputClass}
+            type="tel"
           />
+
+          {errors?.address?.phone && (
+            <p className="text-red-500 text-sm">
+              {errors.address.phone.message}
+            </p>
+          )}
+
           <input
             {...register("address.city")}
-            placeholder="city"
+            placeholder="City"
             className={inputClass}
+            required
           />
           <input
             {...register("address.upozilla")}
-            placeholder="upozilla"
+            placeholder="Upozilla"
             className={inputClass}
+            required
           />
-          <div className="grid grid-cols-3 gap-3">
-            <p>cash on delivery</p>
-            <p>Bkash</p>
-            <p>Nagad</p>
+
+          <h3 className="text-xl font-semibold mt-4 text-text">
+            Payment Method
+          </h3>
+
+          {/* Payment Methods */}
+          <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+            {/* Cash */}
+            <label className="flex  gap-1 p-2  bg-gray-100 items-center border border-gray-400  rounded cursor-pointer hover:shadow-md transition">
+              <input
+                type="radio"
+                value="Cash"
+                {...register("paymentMethod")}
+                className="mb-2"
+                defaultChecked
+              />
+              <img src={cash} className="w-8 h-8 mb-1" alt="" />
+              <span className="text-gray-700 text-sm">COD</span>
+            </label>
+
+            {/* Bkash */}
+            <label className="flex  gap-1 p-2  bg-gray-100 items-center border border-gray-400  rounded cursor-pointer hover:shadow-md transition">
+              <input
+                type="radio"
+                value="Bkash"
+                {...register("paymentMethod")}
+              />
+              <img src={bkash} className="w-8 h-8 " alt="" />
+              <span className="text-gray-700 text-sm">Bkash</span>
+            </label>
+
+            {/* Nagad */}
+            <label className="flex  gap-1 p-2  bg-gray-100 items-center border border-gray-400  rounded cursor-pointer hover:shadow-md transition">
+              <input
+                type="radio"
+                value="Nagad"
+                {...register("paymentMethod")}
+                className="mb-2"
+              />
+              <img src={nagad} className="w-7 h-7 mb-1" alt="" />
+              <span className="text-gray-700 text-sm">Nagad</span>
+            </label>
           </div>
 
           <button
             type="submit"
-            className="w-full bg-primary text-white py-3 rounded-xl font-semibold mt-4 hover:bg-primary/90 transition"
+            className="w-full bg-primary text-white py-3  font-semibold mt-4 hover:bg-primary/90 transition"
           >
-            Pay Now
+            Confirm Order
           </button>
         </form>
 
         {/* Cart Summary */}
-        <div className="bg-white rounded-2xl shadow-lg p-6 flex flex-col sticky top-5 space-y-4">
-          <h2 className="text-xl font-semibold text-gray-800">
-            Review your cart
-          </h2>
+        <div className="bg-white rounded shadow p-6 flex flex-col sticky top-5 space-y-4">
+          <h2 className="text-xl font-semibold text-text">Review your cart</h2>
+
           <div className="flex-1 space-y-4 max-h-[500px] overflow-y-auto">
             {cartItems.map((item) => (
               <div key={item._id} className="flex items-center gap-4">
@@ -128,10 +188,10 @@ const OrderPage = () => {
                   className="w-20 h-20 rounded object-cover"
                 />
                 <div className="flex-1">
-                  <h3 className="text-gray-800 font-semibold">{item.name}</h3>
+                  <h3 className="text-text font-semibold">{item.name}</h3>
                   <p className="text-gray-500">{item.qty}x</p>
                 </div>
-                <p className="text-gray-800 font-semibold">
+                <p className="text-text font-semibold">
                   ৳{Math.ceil((item.discountPrice || item.price) * item.qty)}
                 </p>
               </div>
@@ -148,7 +208,8 @@ const OrderPage = () => {
             <p className="flex justify-between text-gray-700">
               Discount <span>৳0</span>
             </p>
-            <p className="flex justify-between font-semibold text-gray-800 text-lg border-t pt-2">
+
+            <p className="flex justify-between font-semibold text-text text-lg border-t pt-2">
               Total <span>৳{Math.ceil(totalPrice + 130)}</span>
             </p>
           </div>
